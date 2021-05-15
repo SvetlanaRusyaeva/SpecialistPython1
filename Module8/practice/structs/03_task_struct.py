82709f54c32a9320cdba5db27396707398aec121
SvetlanaRusyaeva <83123872+SvetlanaRusyaeva@users.noreply.github.com>
2021-05-15 15:06:26 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,11 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+lst = [1, -3, 0, 4,-8,15,-4,16,10]
+kol = len(list(filter(lambda number: number <= 10, lst)))
+print(kol)
+positiv_sum = sum(list(filter(lambda number: number > 0, lst)))
+print(positiv_sum)
+lst2 = list(filter(lambda number: number%2==0, lst))
+srednee = sum(lst2)/len(lst2)
+print(srednee)
