068ab248659cec3d97caa83a4a776e17f596892e
SvetlanaRusyaeva <83123872+SvetlanaRusyaeva@users.noreply.github.com>
2021-05-15 15:13:40 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,6 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+dictionary = dictionary_1.copy()
+dictionary.update(dictionary_2)
+print(dictionary)
